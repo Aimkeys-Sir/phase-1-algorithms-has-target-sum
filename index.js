ab50a65e0e11df6e8488d19003f4d6fc4b1828cd
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-19 10:09:30 +0300

    commit tree 727e7ca0193bd163597b7e231472bdaf58d9f8d5

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for(let i=0;i<array.length;i++)
+  {
+    for(let j=i+1;j<array.length;j++)
+    {
+      //console.log(`${array[i]}+ ${array[j]}=${array[i]+array[j]}`)
+      if(array[i]+array[j]==target)
+      {
+        return true
+      }
+    }
+    
+  }
+return false
 
+}
+console.log(`it is ${hasTargetSum([22, 19, 4, 6, 30], 25)}`)
 /* 
   Write the Big O time complexity of your function here
+  how many times will the for loops run?
+  for 3 numbers
+  a+b
+  a+c
+  b+c ==3
+  for 4 num
+  a+e
+  b+e
+  c+e ==6
+  for 5
+  a+f
+  b+f
+  c+f
+  e+f==10
+so the formula
+ input!/(input-2)!2!==> (input)Combination(2)
+
+ O(nC2)
 */
 
 /* 
   Add your pseudocode here
+  3+8,
+  3+12,
+  3+4,
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
